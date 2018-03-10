eb85112926d7da7d3e62708a525cd9ea9bd51bc3
Charles W. Roberts <Charles.Roberts@gatech.edu>
2018-03-10 06:56:08 -0500

    Edit Heroku Config

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "charles",
-  password: "it's a secret",
-  database: "burgers_db"
-});
+var connection;
+
+
+if(process.env.JAWSDB_URL)
+{
+  if (process.env.JAWSDB_URL)
+  {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else
+  {
+    connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "charles",
+    password: "it's a secret",
+    database: "burgers_db"
+  });
+  
+  }
 
 // Make connection.
 connection.connect(function(err) {
@@ -24,8 +37,10 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+
 });
+
+}
 
 // Export connection for our ORM to use.
 module.exports = connection;
